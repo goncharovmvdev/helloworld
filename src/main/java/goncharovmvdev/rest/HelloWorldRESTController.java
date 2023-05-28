94ecfa6e7c4efaa30e5e04bb1fdee0130e51c016
goncharovmvdev <goncharovmv.dev@gmail.com>
2023-05-28 19:51:02 +0200

    fix:   - HelloWorldRESTController#helloWorldGET (optional)@PathVariable(value = "name", required = false) String name -> @RequestParam(name = "name", required = false) String name add:   - api versioning (see application.yaml, HelloWorldRESTController)

--- src/main/java/goncharovmvdev/rest/HelloWorldRESTController.java
+++ src/main/java/goncharovmvdev/rest/HelloWorldRESTController.java
@@ -1,28 +1,25 @@
 package goncharovmvdev.rest;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author gonch
  * @since 5/28/2023, 7:24 PM
  */
 @RestController
-@RequestMapping("${uris.root:/}")
+@RequestMapping("${uris.api}${uris.greeting}")
 public class HelloWorldRESTController {
     private final String greeting;
 
-    public HelloWorldRESTController(@Value("greeting") String greeting) {
+    public HelloWorldRESTController(@Value("${greeting}") String greeting) {
         this.greeting = greeting;
     }
 
-    @GetMapping("/{name}")
-    public String helloWorld(@PathVariable(value = "name", required = false) String name) {
+    @GetMapping("/v0")
+    public String helloWorldGET(@RequestParam(value = "name", required = false) String name) {
         return name == null
                 ? greeting + ", World!"
-                : greeting + ", " + name;
+                : greeting + ", " + name + "!";
     }
 }
